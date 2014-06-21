ee290f7cad32302045c7918d90a448710a2746a7
Rogério Brito <rbrito@ime.usp.br>
2014-06-21 00:29:03 -0300

    Reformat Java files for legibility.
    Signed-off-by: Rogério Brito <rbrito@ime.usp.br>

--- src/com/hlidskialf/android/pomodoro/PomodoroActivity.java
+++ src/com/hlidskialf/android/pomodoro/PomodoroActivity.java
@@ -17,183 +17,173 @@ import android.widget.LinearLayout;
 
 import com.hlidskialf.android.widget.CountDownView;
 
-public class PomodoroActivity extends Activity
-{
-  private CountDownView mTimerView;
-  private TextView mStatusText;
-  private ViewGroup mTomatoBar;
-  private Button mStartButton, mStopButton;
+public class PomodoroActivity extends Activity {
+	private CountDownView mTimerView;
+	private TextView mStatusText;
+	private ViewGroup mTomatoBar;
+	private Button mStartButton, mStopButton;
 
-  private SharedPreferences mPrefs;
-  
-  private int mCurAlarmType,mTomatoCount;
+	private SharedPreferences mPrefs;
 
-  private static final int REQUEST_PREFERENCES=1;
+	private int mCurAlarmType, mTomatoCount;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+	private static final int REQUEST_PREFERENCES = 1;
 
-    mTimerView = (CountDownView)findViewById(R.id.tomato_clock);
-    mStatusText = (TextView)findViewById(R.id.tomato_status);
-    mTomatoBar = (ViewGroup)findViewById(R.id.tomato_bar);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-    mStartButton = (Button)findViewById(R.id.tomato_start);
-    mStartButton.setOnClickListener(new Button.OnClickListener() {
-      public void onClick(View b) { 
-        mPrefs = getSharedPreferences(Pomodoro.PREFERENCES, 0);
-        int duration = mPrefs.getInt(Pomodoro.PREF_TOMATO_DURATION, Pomodoro.PREF_TOMATO_DURATION_DEFAULT);
-        
-        Pomodoro.startTomato(PomodoroActivity.this);
-        mTimerView.start(duration*60000);
-        mCurAlarmType = Pomodoro.ALARM_TYPE_TOMATO;
+		mTimerView = (CountDownView) findViewById(R.id.tomato_clock);
+		mStatusText = (TextView) findViewById(R.id.tomato_status);
+		mTomatoBar = (ViewGroup) findViewById(R.id.tomato_bar);
 
-        update_tomato_bar();
-        update_status();
-      }
-    });
+		mStartButton = (Button) findViewById(R.id.tomato_start);
+		mStartButton.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View b) {
+				mPrefs = getSharedPreferences(Pomodoro.PREFERENCES, 0);
+				int duration = mPrefs.getInt(Pomodoro.PREF_TOMATO_DURATION,
+						Pomodoro.PREF_TOMATO_DURATION_DEFAULT);
 
+				Pomodoro.startTomato(PomodoroActivity.this);
+				mTimerView.start(duration * 60000);
+				mCurAlarmType = Pomodoro.ALARM_TYPE_TOMATO;
 
-    mStopButton = (Button)findViewById(R.id.tomato_stop);
-    mStopButton.setOnClickListener(new Button.OnClickListener() {
-      public void onClick(View b) { 
-        Pomodoro.stopTomato(PomodoroActivity.this);
-        mTimerView.stop();
+				update_tomato_bar();
+				update_status();
+			}
+		});
 
-        if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST)
-          mTomatoCount = Pomodoro.setTomatoCount(PomodoroActivity.this, -1);
+		mStopButton = (Button) findViewById(R.id.tomato_stop);
+		mStopButton.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View b) {
+				Pomodoro.stopTomato(PomodoroActivity.this);
+				mTimerView.stop();
 
-        mCurAlarmType = Pomodoro.ALARM_TYPE_NONE;
+				if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST)
+					mTomatoCount = Pomodoro.setTomatoCount(
+							PomodoroActivity.this, -1);
 
-        update_tomato_bar();
-        update_status();
-      }
-    });
+				mCurAlarmType = Pomodoro.ALARM_TYPE_NONE;
 
-  }
-  @Override
-  public void onResume()
-  {
-    super.onResume();
+				update_tomato_bar();
+				update_status();
+			}
+		});
 
-    mPrefs = getSharedPreferences(Pomodoro.PREFERENCES, 0);
-    long start = mPrefs.getLong(Pomodoro.PREF_ALARM_START,0);
-    long duration = mPrefs.getLong(Pomodoro.PREF_ALARM_DURATION,0);
-    mCurAlarmType = mPrefs.getInt(Pomodoro.PREF_ALARM_TYPE,0); 
-    mTomatoCount = mPrefs.getInt(Pomodoro.PREF_TOMATO_COUNT,0);
+	}
 
-    mTimerView.stop();
-    if (mCurAlarmType == Pomodoro.ALARM_TYPE_TOMATO) {
-      mTimerView.start(duration,start);
-    }
-    else
-    if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST) {
-      mTimerView.start(duration,start);
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
 
-    update_tomato_bar();
-    update_status();
-  }
-  @Override
-  public void onPause()
-  {
-    super.onPause();
-    mTimerView.pause();
-  }
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
-    super.onCreateOptionsMenu(menu);
-    getMenuInflater().inflate(R.menu.options, menu);
+		mPrefs = getSharedPreferences(Pomodoro.PREFERENCES, 0);
+		long start = mPrefs.getLong(Pomodoro.PREF_ALARM_START, 0);
+		long duration = mPrefs.getLong(Pomodoro.PREF_ALARM_DURATION, 0);
+		mCurAlarmType = mPrefs.getInt(Pomodoro.PREF_ALARM_TYPE, 0);
+		mTomatoCount = mPrefs.getInt(Pomodoro.PREF_TOMATO_COUNT, 0);
 
-    return true;
-  }
-  @Override
-  public boolean onPrepareOptionsMenu(Menu menu)
-  {
-    MenuItem reset_item = menu.findItem(R.id.options_menu_reset);
-    reset_item.setVisible(Pomodoro.getTomatoCount(this) > 0);
-    return super.onPrepareOptionsMenu(menu);
-  }
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    switch (item.getItemId()) {
-      case R.id.options_menu_preferences:
-        startActivityForResult( new Intent(this, PomodoroPreferences.class), REQUEST_PREFERENCES );
-        return true;
-      case R.id.options_menu_about:
-        View layout = getLayoutInflater().inflate(R.layout.about, null);
-        AlertDialog dia = new AlertDialog.Builder(this)
-                                .setTitle(R.string.about_title)
-                                .setView(layout)
-                                .setPositiveButton(android.R.string.ok, null)
-                                .show();
-        return true;
-      case R.id.options_menu_reset:
-        Pomodoro.stopTomato(PomodoroActivity.this);
-        mTomatoCount = Pomodoro.setTomatoCount(PomodoroActivity.this, 0);
-        update_tomato_bar();
-        update_status();
-        return true;
-    }    
-    return super.onOptionsItemSelected(item);
-  }
-  
+		mTimerView.stop();
+		if (mCurAlarmType == Pomodoro.ALARM_TYPE_TOMATO) {
+			mTimerView.start(duration, start);
+		} else if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST) {
+			mTimerView.start(duration, start);
+		}
 
-  private void update_status()
-  {
-    if (mCurAlarmType == Pomodoro.ALARM_TYPE_TOMATO) {
-      mStatusText.setText(R.string.status_tomato);
-      mStartButton.setEnabled(false);
-      mStopButton.setVisibility(View.VISIBLE);
-    }
-    else
-    if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST) {
-      mStatusText.setText(R.string.status_rest);
-      mStartButton.setEnabled(false);
-      mStopButton.setVisibility(View.VISIBLE);
-    }
-    else {
-      mStatusText.setText(R.string.status_none);
-      mStartButton.setEnabled(true);
-      mStopButton.setVisibility(View.GONE);
-    }
+		update_tomato_bar();
+		update_status();
+	}
 
-  }
+	@Override
+	public void onPause() {
+		super.onPause();
+		mTimerView.pause();
+	}
 
-  private void update_tomato_bar()
-  {
-    mTomatoBar.removeAllViews();
-    int i;
-    int c=0;
-    LinearLayout holder = null;
-    for (i=0; i < mTomatoCount; i++) {
-      if (c++ % 4 == 0) {
-        holder = new LinearLayout(this);
-        holder.setGravity(Gravity.CENTER);
-        mTomatoBar.addView(holder);
-      }
-      ImageView iv = new ImageView(this);
-      iv.setImageResource(R.drawable.tomato);
-      holder.addView(iv);
-    }
-    if (c++ % 4 == 0) {
-      holder = new LinearLayout(this);
-      holder.setGravity(Gravity.CENTER);
-      mTomatoBar.addView(holder);
-    }
-    if (mCurAlarmType == Pomodoro.ALARM_TYPE_TOMATO) {
-      ImageView iv = new ImageView(this);
-      iv.setImageResource(R.drawable.greentomato);
-      holder.addView(iv);
-    }
-    if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST) {
-      ImageView iv = new ImageView(this);
-      iv.setImageResource(R.drawable.tomato);
-      holder.addView(iv);
-    }
-  }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.options, menu);
+
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem reset_item = menu.findItem(R.id.options_menu_reset);
+		reset_item.setVisible(Pomodoro.getTomatoCount(this) > 0);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.options_menu_preferences:
+			startActivityForResult(new Intent(this, PomodoroPreferences.class),
+					REQUEST_PREFERENCES);
+			return true;
+		case R.id.options_menu_about:
+			View layout = getLayoutInflater().inflate(R.layout.about, null);
+			AlertDialog dia = new AlertDialog.Builder(this)
+					.setTitle(R.string.about_title).setView(layout)
+					.setPositiveButton(android.R.string.ok, null).show();
+			return true;
+		case R.id.options_menu_reset:
+			Pomodoro.stopTomato(PomodoroActivity.this);
+			mTomatoCount = Pomodoro.setTomatoCount(PomodoroActivity.this, 0);
+			update_tomato_bar();
+			update_status();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void update_status() {
+		if (mCurAlarmType == Pomodoro.ALARM_TYPE_TOMATO) {
+			mStatusText.setText(R.string.status_tomato);
+			mStartButton.setEnabled(false);
+			mStopButton.setVisibility(View.VISIBLE);
+		} else if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST) {
+			mStatusText.setText(R.string.status_rest);
+			mStartButton.setEnabled(false);
+			mStopButton.setVisibility(View.VISIBLE);
+		} else {
+			mStatusText.setText(R.string.status_none);
+			mStartButton.setEnabled(true);
+			mStopButton.setVisibility(View.GONE);
+		}
+
+	}
+
+	private void update_tomato_bar() {
+		mTomatoBar.removeAllViews();
+		int i;
+		int c = 0;
+		LinearLayout holder = null;
+		for (i = 0; i < mTomatoCount; i++) {
+			if (c++ % 4 == 0) {
+				holder = new LinearLayout(this);
+				holder.setGravity(Gravity.CENTER);
+				mTomatoBar.addView(holder);
+			}
+			ImageView iv = new ImageView(this);
+			iv.setImageResource(R.drawable.tomato);
+			holder.addView(iv);
+		}
+		if (c++ % 4 == 0) {
+			holder = new LinearLayout(this);
+			holder.setGravity(Gravity.CENTER);
+			mTomatoBar.addView(holder);
+		}
+		if (mCurAlarmType == Pomodoro.ALARM_TYPE_TOMATO) {
+			ImageView iv = new ImageView(this);
+			iv.setImageResource(R.drawable.greentomato);
+			holder.addView(iv);
+		}
+		if (mCurAlarmType == Pomodoro.ALARM_TYPE_REST) {
+			ImageView iv = new ImageView(this);
+			iv.setImageResource(R.drawable.tomato);
+			holder.addView(iv);
+		}
+	}
 }
--- src/com/hlidskialf/android/pomodoro/PomodoroPreferences.java
+++ src/com/hlidskialf/android/pomodoro/PomodoroPreferences.java
@@ -1,5 +1,5 @@
-
 package com.hlidskialf.android.pomodoro;
+
 import android.preference.PreferenceActivity;
 import android.preference.Preference;
 import android.content.SharedPreferences;
@@ -8,71 +8,81 @@ import java.util.HashMap;
 
 import com.hlidskialf.android.preference.SliderPreference;
 
-public class PomodoroPreferences extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener
-{
-  private SharedPreferences mPrefs;
-  private SliderPreference mTomatoDuration, mRestDuration;
+public class PomodoroPreferences extends PreferenceActivity implements
+		SharedPreferences.OnSharedPreferenceChangeListener {
+	private SharedPreferences mPrefs;
+	private SliderPreference mTomatoDuration, mRestDuration;
 
-  private HashMap<String, Preference> mPreferences;
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	private HashMap<String, Preference> mPreferences;
 
-    addPreferencesFromResource(R.xml.preferences);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    mPrefs = getPreferenceScreen().getSharedPreferences();
+		addPreferencesFromResource(R.xml.preferences);
 
-    mPreferences = new HashMap<String,Preference>();
+		mPrefs = getPreferenceScreen().getSharedPreferences();
 
-    Preference pref;
-    pref = findPreference(Pomodoro.PREF_REST_DURATION);
-    fillInValue(pref, String.valueOf( mPrefs.getInt(Pomodoro.PREF_REST_DURATION, Pomodoro.PREF_REST_DURATION_DEFAULT) ));
-    mPreferences.put(Pomodoro.PREF_REST_DURATION, pref);
-    pref = findPreference(Pomodoro.PREF_REST_VOLUME);
-    fillInValue(pref, String.valueOf( mPrefs.getInt(Pomodoro.PREF_REST_VOLUME, Pomodoro.PREF_REST_VOLUME_DEFAULT) ));
-    mPreferences.put(Pomodoro.PREF_REST_VOLUME, pref);
-    pref = findPreference(Pomodoro.PREF_REST_DELAY);
-    fillInValue(pref, String.valueOf( mPrefs.getInt(Pomodoro.PREF_REST_DELAY, Pomodoro.PREF_REST_DELAY_DEFAULT) ));
-    mPreferences.put(Pomodoro.PREF_REST_DELAY, pref);
-    pref = findPreference(Pomodoro.PREF_TOMATO_DURATION);
-    fillInValue(pref, String.valueOf( mPrefs.getInt(Pomodoro.PREF_TOMATO_DURATION, Pomodoro.PREF_TOMATO_DURATION_DEFAULT) ));
-    mPreferences.put(Pomodoro.PREF_TOMATO_DURATION, pref);
-    pref = findPreference(Pomodoro.PREF_TOMATO_VOLUME);
-    fillInValue(pref, String.valueOf( mPrefs.getInt(Pomodoro.PREF_TOMATO_VOLUME, Pomodoro.PREF_TOMATO_VOLUME_DEFAULT) ));
-    mPreferences.put(Pomodoro.PREF_TOMATO_VOLUME, pref);
-    pref = findPreference(Pomodoro.PREF_TOMATO_DELAY);
-    fillInValue(pref, String.valueOf( mPrefs.getInt(Pomodoro.PREF_TOMATO_DELAY, Pomodoro.PREF_TOMATO_DELAY_DEFAULT) ));
-    mPreferences.put(Pomodoro.PREF_TOMATO_DELAY, pref);
+		mPreferences = new HashMap<String, Preference>();
 
-  }
-  @Override
-  protected void onResume() {
-    super.onResume();
-    mPrefs.registerOnSharedPreferenceChangeListener(this);
-  }
-  @Override
-  protected void onPause() {
-    super.onPause();
-    mPrefs.unregisterOnSharedPreferenceChangeListener(this);
-  }
-  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
-  {
-    Preference pref = mPreferences.get(key);
+		Preference pref;
+		pref = findPreference(Pomodoro.PREF_REST_DURATION);
+		fillInValue(pref, String.valueOf(mPrefs.getInt(
+				Pomodoro.PREF_REST_DURATION,
+				Pomodoro.PREF_REST_DURATION_DEFAULT)));
+		mPreferences.put(Pomodoro.PREF_REST_DURATION, pref);
+		pref = findPreference(Pomodoro.PREF_REST_VOLUME);
+		fillInValue(pref, String.valueOf(mPrefs.getInt(
+				Pomodoro.PREF_REST_VOLUME, Pomodoro.PREF_REST_VOLUME_DEFAULT)));
+		mPreferences.put(Pomodoro.PREF_REST_VOLUME, pref);
+		pref = findPreference(Pomodoro.PREF_REST_DELAY);
+		fillInValue(pref, String.valueOf(mPrefs.getInt(
+				Pomodoro.PREF_REST_DELAY, Pomodoro.PREF_REST_DELAY_DEFAULT)));
+		mPreferences.put(Pomodoro.PREF_REST_DELAY, pref);
+		pref = findPreference(Pomodoro.PREF_TOMATO_DURATION);
+		fillInValue(pref, String.valueOf(mPrefs.getInt(
+				Pomodoro.PREF_TOMATO_DURATION,
+				Pomodoro.PREF_TOMATO_DURATION_DEFAULT)));
+		mPreferences.put(Pomodoro.PREF_TOMATO_DURATION, pref);
+		pref = findPreference(Pomodoro.PREF_TOMATO_VOLUME);
+		fillInValue(pref, String.valueOf(mPrefs.getInt(
+				Pomodoro.PREF_TOMATO_VOLUME,
+				Pomodoro.PREF_TOMATO_VOLUME_DEFAULT)));
+		mPreferences.put(Pomodoro.PREF_TOMATO_VOLUME, pref);
+		pref = findPreference(Pomodoro.PREF_TOMATO_DELAY);
+		fillInValue(pref,
+				String.valueOf(mPrefs.getInt(Pomodoro.PREF_TOMATO_DELAY,
+						Pomodoro.PREF_TOMATO_DELAY_DEFAULT)));
+		mPreferences.put(Pomodoro.PREF_TOMATO_DELAY, pref);
 
-    if (pref != null)
-      fillInValue(pref, String.valueOf( sharedPreferences.getInt(key, 0) ));
-  }
+	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mPrefs.registerOnSharedPreferenceChangeListener(this);
+	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mPrefs.unregisterOnSharedPreferenceChangeListener(this);
+	}
 
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		Preference pref = mPreferences.get(key);
 
-  private static void fillInValue(Preference pref, String value)
-  {
-    String summary = pref.getSummary().toString();
-    int idx = summary.lastIndexOf('=');
-    if (idx != -1) {
-      summary = summary.substring(0,idx+1).concat( " "+value );
-    }
-    pref.setSummary(summary);
-  }
+		if (pref != null)
+			fillInValue(pref, String.valueOf(sharedPreferences.getInt(key, 0)));
+	}
+
+	private static void fillInValue(Preference pref, String value) {
+		String summary = pref.getSummary().toString();
+		int idx = summary.lastIndexOf('=');
+		if (idx != -1) {
+			summary = summary.substring(0, idx + 1).concat(" " + value);
+		}
+		pref.setSummary(summary);
+	}
 }
